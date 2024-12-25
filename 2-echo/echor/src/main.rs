ee5ebf34b560e3b2a14e4311170c495fcb7f301f
yuchiki <14884013+yuchiki@users.noreply.github.com>
2024-12-25 17:08:43 +0900

    body of echor

--- 2-echo/echor/src/main.rs
+++ 2-echo/echor/src/main.rs
@@ -1,3 +1,30 @@
+extern crate clap;
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("echor")
+        .version("0.1.0")
+        .author("Yuchiki")
+        .about("Rust Echo")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .num_args(1..),
+
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do not output the trailing newline")
+                .num_args(0),
+        )
+        .get_matches();
+
+    let text: Vec::<String> = matches.get_many::<String>("text").unwrap().cloned().collect();
+    let omit_newline = matches.get_flag("omit_newline");
+    let ending = if omit_newline { "" } else { "\n" };
+
+    println!("{}{}", text.join(" "), ending);
 }
